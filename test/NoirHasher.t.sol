1fd3c54648b511321e1988a9b818a49afeaf2860
Alex Roan <alex.roan@hotmail.com>
2023-07-26 12:43:03 +0100

    Sha commitment hasher

--- test/NoirHasher.t.sol
+++ test/NoirHasher.t.sol
@@ -6,13 +6,21 @@ import "forge-std/test.sol";
 contract NoirHasherTest is Test {
 
     function setUp() public {
-        
     }
 
-    function testSha256Hash() public {
+    function test_sha256One() public {
         bytes memory number = abi.encode(0);
         console.logBytes(number);
         bytes32 hashed = sha256(number);
+        console.logBytes32(hashed);
+    }
+
+    function test_sha256Two() public {
+        bytes memory number0 = abi.encode(0);
+        bytes memory number1 = abi.encode(1);
+        bytes memory combined = bytes.concat(number0, number1);
+        console.logBytes(combined);
+        bytes32 hashed = sha256(combined);
         console.logBytes32(hashed);
     }
 }
